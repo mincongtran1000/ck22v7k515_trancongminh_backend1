b1e994f0bec995445aab6edc58a87029e61b7aec
mincongtran1000 <mincongtran1804@gmail.com>
2024-03-09 11:45:24 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,23 @@ app.use(express.json());
 app.get("/", (req, res) => {
 res.json({ message: "Welcome to contact book application." });
 });
- app.use("/api/contacts", contactsRouter);
+
+ module.exports = app;
+ const ApiError = require("./app/api-error");
+  app.use("/api/contacts", contactsRouter);
+  // handle 404 response
+  app.use((req, res, next) => {
+ // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+ // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+ return next(new ApiError(404, "Resource not found"));
+ });
+  // define error-handling middleware last, after other app.use() and routes calls
+ app.use((err, req, res) => {
+ // Middleware xử lý lỗi tập trung.
+ // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+return res.status(error.statusCode || 500).json({
+     message: error.message || "Internal Server Error",});
 // eslint-disable-next-line no-undef
-module.exports = app;
+ })
+
+
